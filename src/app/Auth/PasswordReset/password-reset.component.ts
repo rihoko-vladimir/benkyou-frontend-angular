1e4be487a3dbb85cd2476d6f2decb6884ac03498
Vladimir <vovakozlouskiy@gmail.com>
2022-06-28 16:52:40 +0300

    Password reset component is finished

--- src/app/Auth/PasswordReset/password-reset.component.ts
+++ src/app/Auth/PasswordReset/password-reset.component.ts
@@ -1,4 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
+import {FormControl, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {MatStepper} from "@angular/material/stepper";
 
 @Component({
   selector: "password-reset",
@@ -7,5 +10,37 @@ import {Component} from "@angular/core";
 })
 
 export class PasswordResetComponent{
+  emailControl = new FormControl("",
+    [Validators.required, Validators.email])
 
+  @ViewChild("stepper") stepper: MatStepper | any
+
+  constructor(private router: Router) {
+  }
+
+  getEmailErrorMessage(){
+    if (this.emailControl.hasError(Validators.required.name))
+      return "This field is required to continue :P"
+
+    if (this.emailControl.hasError(Validators.email.name))
+      return "Incorrect email address provided"
+
+    return "Unknown error occurred"
+  }
+
+  async onCancelClicked(){
+    await this.router.navigate(["auth"])
+  }
+
+  onNextClicked() {
+    if (this.emailControl.valid){
+      this.stepper.next()
+    }else{
+      this.emailControl.markAsTouched()
+    }
+  }
+
+  async onFinishClicked(){
+    await this.router.navigate(["auth"])
+  }
 }
