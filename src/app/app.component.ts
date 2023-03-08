812918df338f536c4cd018fee8744c2685c631b0
Uladzimir Kazlouski <vovakozlouskiy@gmail.com>
2023-03-08 20:53:34 +0300

    Implemented basic animations into application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {ChildrenOutletContexts} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private contexts: ChildrenOutletContexts) {
+  }
+  getAppRoutingAnimations(){
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
+}
